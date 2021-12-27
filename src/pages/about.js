ebe6531bfde7848fc4fdc9b9b5863d7afa7b6434
sandorfalot <sandorfalot@gmail.com>
2021-12-27 17:58:42 -0500

    First commit

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+// import react
+import * as React from 'react'
+import Layout from '../components/layout'
+
+// define component
+const AboutPage = () => {
+    return (
+        <Layout pageTitle="About Me">
+            <p>Hi, I'm San!</p>
+        </Layout>
+    )
+}
+
+export default AboutPage
